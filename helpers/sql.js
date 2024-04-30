4a6afc1ab51eaf9b21fe7534892ffa458e894a43
Nicholas <NLoo1@outlook.com>
2024-04-30 17:56:31 -0400

    Document sql.js

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,16 +1,35 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/**
+* Takes updated data and JSON to return new JSON containing updated values.
+* Used to create a partial SQL update.
+*
+* @param {Object} dataToUpdate JSON object containing updated data
+* @param {Object} jsToSql Takes JSON object of columns to update
+* @returns {Object} JSON with String of new cols to set/update and updated data values.
+*/
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+
+  // In dataToUpdate, get the keys.
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
+
+
+  // Using keys, cols is a map of dataToUpdate.
+  // The key is named using matching keys in jsToSql.
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
   const cols = keys.map((colName, idx) =>
       `"${jsToSql[colName] || colName}"=$${idx + 1}`,
   );
 
+
+  // {
+  //   setCols: '"first_name"=$1, "last_name"=$2, "age"=$3, "email"=$4',
+  //   values: ['John', 'Doe', 30, 'john.doe@example.com']
+  // }
+  
   return {
     setCols: cols.join(", "),
     values: Object.values(dataToUpdate),
